5fd3ad351bc3fce3c5109b8d6eeb3130a79522aa
leixiaohui-1974 <leixiaohui@gmail.com>
2025-10-03 17:00:24 +0800

    清零

---
+++ physical_objects/__init__.py
@@ -0,0 +1,6 @@
+﻿"""Physical objects package for WaterNet."""
+
+from .integral_delay_canal import IntegralDelayCanal
+from .reservoir import Reservoir
+
+__all__ = ["Reservoir", "IntegralDelayCanal"]
